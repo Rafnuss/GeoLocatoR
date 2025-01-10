e3593c5092cd778b9cafbf2ee4454c91fb971985
Raphaël Nussbaumer <rafnuss@gmail.com>
2025-01-10 23:34:28 +0100

    Add tests

---
+++ tests/testthat/test-create_gldp.R
@@ -0,0 +1,72 @@
+library(testthat)
+library(glue)
+library(GeoLocatoR)
+
+# Test for create_gldp function
+test_that("create_gldp creates a valid GeoLocator Data Package", {
+  # Test minimal input
+  package <- create_gldp(
+    title = "Geolocator Data Package example",
+    contributors = list(
+      list(
+        title = "Raphaël Nussbaumer",
+        roles = c("ContactPerson", "DataCurator", "ProjectLeader")
+      )
+    )
+  )
+
+  # Test with more detailed metadata
+  package_full <- create_gldp(
+    title = "Geolocator Data Package example",
+    contributors = list(
+      list(
+        title = "Raphaël Nussbaumer",
+        roles = c("ContactPerson", "DataCurator", "ProjectLeader")
+      ),
+      list(
+        title = "Yann Rime",
+        roles = c("Researcher")
+      )
+    ),
+    id = "https://doi.org/10.5281/zenodo.13829929",
+    licenses = list(list(
+      name = "CC-BY-4.0", title = "Creative Commons Attribution 4.0",
+      path = "https://creativecommons.org/licenses/by/4.0/"
+    )),
+    description = "test",
+    version = "1.0.1",
+    embargo = "2025-01-01",
+    keywords = c("Woodland Kingfisher", "intra-african", "multi-sensor geolocator"),
+    bibliographicCitation   = "Nussbaumer, R.(2024). Woodland Kingfisher (v1.1). Zenodo",
+    grants = c("Swiss National Fundation grant no. 354251"),
+    relatedIdentifiers = list(
+      list(
+        relationType = "IsPartOf",
+        relatedIdentifier = "10.5281/zenodo.11207081",
+        relatedIdentifierType = "DOI"
+      )
+    )
+  )
+})
+
+test_that("create_gldp handles invalid inputs", {
+  # Test invalid title input
+  expect_error(create_gldp(
+    title = 123, # Invalid type
+    contributors = list(list(title = "Raphaël Nussbaumer"))
+  ), "title is not a string")
+
+  # Test invalid contributor input (missing title)
+  expect_error(create_gldp(
+    title = "Valid title",
+    contributors = list(list(roles = c("ContactPerson")))
+  ))
+
+  # Test invalid schema URL
+  expect_error(create_gldp(
+    title = "Valid title",
+    contributors = list(list(title = "Raphaël Nussbaumer")),
+    schema = "invalid_schema_url"
+  ))
+
+})
---
+++ tests/testthat/test-workflow.R
@@ -0,0 +1,22 @@
+library(testthat)
+
+test_that("read_gldp correctly reads and processes GeoLocator Data Package", {
+  pkg <- read_gldp("https://zenodo.org/records/14099115/files/datapackage.json")
+  expect_s3_class(pkg, "geolocatordp")
+
+  version(pkg)
+
+  check_gldp(pkg)
+
+  print(pkg)
+
+  # data manip
+  tags(pkg) <- tags(pkg)
+  observations(pkg) <- observations(pkg)
+  measurements(pkg) <- measurements(pkg)
+  staps(pkg) <- staps(pkg)
+  twilights(pkg) <- twilights(pkg)
+  paths(pkg) <- paths(pkg)
+  edges(pkg) <- edges(pkg)
+  pressurepaths(pkg) <- pressurepaths(pkg)
+})
