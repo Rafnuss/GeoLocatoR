a8af9d70da7a4a128c316807ccbcc16e8ee7f1cc
Raphaël Nussbaumer <rafnuss@gmail.com>
2025-11-25 15:42:30 +0100

    Enhance read_gldp to support Zenodo inputs
    Updated read_gldp to accept Zenodo DOI, DOI URLs, record links, and record numbers, resolving them to the correct datapackage.json file. Expanded tests to cover all supported input types and updated documentation and examples accordingly.

--- R/read_gldp.R
+++ R/read_gldp.R
@@ -7,20 +7,62 @@
 #' ](https://docs.ropensci.org/frictionless/reference/read_package.html) and assigns the class
 #' `"geolocatordp"` to the datapackage read.
 #'
-#' @param file A string specifying the path to the JSON file containing the GeoLocator Data Package
-#' metadata. Defaults to `"datapackage.json"`. Can also be a URL.
+#' @param x A string specifying the path to the JSON file containing the GeoLocator Data Package
+#' metadata. Defaults to `"datapackage.json"`. Also accepts direct urls, Zenodo DOI, Zenodo link, or Zenodo record number.
 #' @param force_read Logical indicating whether to force the reading of the data from path/URL to
 #'   memory. Defaults to `TRUE`.
 #'
 #' @return A GeoLocator Data Package object created from a file/url
 #'
 #' @examples
+#' #' # Local file
+#' read_gldp("datapackage.json")
+## @examples
+#' \dontrun{
+#' # Local file
+#' read_gldp("datapackage.json")
+#' # Zenodo DOI
+#' read_gldp("10.5281/zenodo.15259676")
+#' # Zenodo DOI URL
+#' read_gldp("https://doi.org/10.5281/zenodo.15259676")
+#' # Zenodo record link
+#' read_gldp("https://zenodo.org/records/15259676")
+#' # Zenodo record number
+#' read_gldp("15259676")
+#' }
 #' # Read a datapackage.json file
 #' pkg <- read_gldp("https://zenodo.org/records/14099115/files/datapackage.json")
 #'
 #' pkg
 #' @export
-read_gldp <- function(file = "datapackage.json", force_read = TRUE) {
+read_gldp <- function(x = "datapackage.json", force_read = TRUE) {
+  # If x ends with .json, use as file
+  if (grepl("\\.json$", x)) {
+    file <- x
+  } else {
+    doi_pattern <- "^10\\.5281/zenodo\\.(\\d+)$"
+    doi_url_pattern <- "^https?://doi\\.org/10\\.5281/zenodo\\.(\\d+)$"
+    zenodo_pattern <- "^https?://zenodo\\.org/records/(\\d+)"
+    record_pattern <- "^(\\d{7,})$"
+    rec_id <- NULL
+    if (grepl(doi_pattern, x)) {
+      rec_id <- sub(doi_pattern, "\\1", x)
+    } else if (grepl(doi_url_pattern, x)) {
+      rec_id <- sub(doi_url_pattern, "\\1", x)
+    } else if (grepl(zenodo_pattern, x)) {
+      rec_id <- sub(zenodo_pattern, "\\1", x)
+    } else if (grepl(record_pattern, x)) {
+      rec_id <- sub(record_pattern, "\\1", x)
+    }
+    if (!is.null(rec_id)) {
+      file <- glue::glue(
+        "https://zenodo.org/records/{rec_id}/files/datapackage.json"
+      )
+    } else {
+      file <- x
+    }
+  }
+
   pkg <- frictionless::read_package(file)
 
   # Force the read of the data as
--- tests/testthat/test-read_gldp.R
+++ tests/testthat/test-read_gldp.R
@@ -4,16 +4,27 @@ library(GeoLocatoR)
 # pkg_shared is loaded from setup.R
 
 test_that("read_gldp correctly reads a GeoLocator Data Package", {
-  pkg <- pkg_shared
+  expect_true("title" %in% names(pkg_shared))
+  expect_true("resources" %in% names(pkg_shared))
+  expect_true("contributors" %in% names(pkg_shared))
+})
 
-  # Check class
-  expect_s3_class(pkg, "geolocatordp")
-  expect_s3_class(pkg, "datapackage")
-
-  # Check that it has required fields
-  expect_true("title" %in% names(pkg))
-  expect_true("resources" %in% names(pkg))
-  expect_true("contributors" %in% names(pkg))
+test_that("read_gldp correctly reads a GeoLocator Data Package from all input types", {
+  # Local file
+  # pkg_local <- read_gldp("datapackage.json")
+  # expect_s3_class(pkg_local, "datapackage")
+  # Zenodo DOI
+  pkg_doi <- read_gldp("10.5281/zenodo.17257521")
+  expect_s3_class(pkg_doi, "datapackage")
+  # Zenodo DOI URL
+  pkg_doi_url <- read_gldp("https://doi.org/10.5281/zenodo.17257521")
+  expect_s3_class(pkg_doi_url, "datapackage")
+  # Zenodo record link
+  pkg_link <- read_gldp("https://zenodo.org/records/17257521")
+  expect_s3_class(pkg_link, "datapackage")
+  # Zenodo record number
+  pkg_num <- read_gldp("17257521")
+  expect_s3_class(pkg_num, "datapackage")
 })
 
 test_that("read_gldp handles invalid input", {
