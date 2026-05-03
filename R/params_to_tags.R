506dbb2f70f5d0a58ac3aea94376de7a3f264e00
Raphaël Nussbaumer <rafnuss@gmail.com>
2026-05-03 16:00:07 +0200

    Add fallback from graph_set_movement

--- R/params_to_tags.R
+++ R/params_to_tags.R
@@ -39,7 +39,8 @@ params_to_tags <- function(params) {
         tibble::tibble(
           tag_id = param$id,
           manufacturer = param$tag_create$manufacturer,
-          scientific_name = param$bird_create$scientific_name,
+          scientific_name = param$bird_create$scientific_name %||%
+            param$graph_set_movement$bird$scientific_name,
           ring_number = NA_character_
         )
       },
